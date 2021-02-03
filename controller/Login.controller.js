ac67f3776af463ea89257961076c3c86dfdbcac3
abuchi1 <hoangminhquan90@gmail.com>
2021-02-03 15:55:04 +0700

    hàm register theo promise

--- controller/Login.controller.js
+++ controller/Login.controller.js
@@ -87,46 +87,69 @@ module.exports.logout = (req, res) => {
 };
 
 // Register
-module.exports.register = async (req, res) => {
+module.exports.register = (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) {
     res.status(400).json({
-      code: 400,
       success: false,
-      message: 'Data error',
-      error: error.details[0].message,
+      message: error.message
     });
   } else {
-    const { username, password, studentID } = req.body;
-    const reg = await utils.register(username, password, studentID);
-    if (reg.code === 409) {
-      res.status(409).json({
-        code: 409,
-        success: false,
-        message: 'Username already exist',
-      });
-    } else if (reg.code === 404) {
-      res.status(404).json({
-        code: 404,
-        success: false,
-        message: 'Student not fault',
-      });
-    } else if (reg.code === 200) {
-      res.status(200).json({
-        code: 200,
-        success: true,
-        data: reg.data,
-      });
-    } else if (reg.code === 400) {
-      res.status(400).json({
-        code: 400,
-        success: false,
-        message: 'Can not create new account. Syntax error',
-        error: reg.data,
-      });
-    }
+    utils.registerPromise(req.body).then(user => {
+      delete user.password;
+      res.status(200).json({ success: true, data: user });
+    }).catch(e => {
+      let resp = Object.assign({ success: false }, { code: e.code, message: e.message || e.messageDev });
+      res.status(400).json(resp);
+    });
   }
 };
+// module.exports.register = async (req, res) => {
+//   const { error } = registerValidation(req.body);
+//   if (error) {
+//     /**
+//      *  không phải error code của http, error code http e đã set ở hàm status rồi. error code ở đây là error của cái lỗi "error ấy". 
+//      *  ví dụ error length của username = 1,
+//      * error length của password = 2,
+//      * thì cái code là 1 or là 2: thì ở client nhận được error code = 1 thì nó sẽ hiện là "độ dài của username phải lớn hơn 6 và nhở hơn 32" ....
+//      */
+//     res.status(400).json({
+//       code: 400, // ở đây error code = 1 || 2
+//       success: false,
+//       message: 'Data error',
+//       error: error.details[0].message,
+//     });
+//   } else {
+//     const { username, password, studentID } = req.body;
+//     const reg = await utils.register(username, password, studentID);
+//     if (reg.code === 409) {
+//       res.status(409).json({
+//         code: 409,
+//         success: false,
+//         message: 'Username already exist',
+//       });
+//     } else if (reg.code === 404) {
+//       res.status(404).json({
+//         code: 404,
+//         success: false,
+//         message: 'Student not fault',
+//       });
+//     } else if (reg.code === 200) {
+//       res.status(200).json({
+//         code: 200,
+//         success: true,
+//         data: reg.data,
+//       });
+//     } else if (reg.code === 400) {
+//       res.status(400).json({
+//         code: 400,
+//         success: false,
+//         message: 'Can not create new account. Syntax error',
+//         error: reg.data,
+//       });
+//     }
+//   }
+// };
 
 // Register admin
 module.exports.adminRegister = async (req, res) => {
--- utils/utils.js
+++ utils/utils.js
@@ -1,20 +1,21 @@
 const bcrypt = require('bcrypt');
-
+const mongoose = require('mongoose');
 const Student = require('../model/student.model');
 const Users = require('../model/users.model');
-
+const Promise = require('bluebird');
 // Update Student
+const isObjectId = id => (mongoose.Types.ObjectId.isValid(id));
 const update = async (id, data) => {
-    try {
-      const updateUser = await Student.findByIdAndUpdate(id, data);
-      if (updateUser) {
-        return {code: 200, data: data}
-      } else {
-        return {code: 404, data: null}
-      }
-    } catch (err) {
-      return {code: 400, data: err}
+  try {
+    const updateUser = await Student.findByIdAndUpdate(id, data);
+    if (updateUser) {
+      return { code: 200, data: data };
+    } else {
+      return { code: 404, data: null };
     }
+  } catch (err) {
+    return { code: 400, data: err };
+  }
 };
 
 // ChangePass User
@@ -22,15 +23,15 @@ const changePass = async (username, password) => {
   try {
     const user = await Users.findOne({ username: username });
     if (!User) {
-      return {code: 404, data: null};
+      return { code: 404, data: null };
     } else {
       const saltRounds = 10;
       const hashPass = await bcrypt.hash(password, saltRounds);
       const updateUser = await user.updateOne({ password: hashPass });
-      return {code: 200, data: updateUser};
+      return { code: 200, data: updateUser };
     }
   } catch (err) {
-    return {code: 400, data: err};
+    return { code: 400, data: err };
   }
 };
 
@@ -44,7 +45,7 @@ const register = async (username, password, studentID) => {
     if (studentID != null) {
       const student = await Student.findById(studentID);
       if (!student) {
-        return {code: 404, data: undefined};
+        return { code: 404, data: undefined };
       }
       const saltRounds = 10;
       const hashPass = await bcrypt.hash(password, saltRounds);
@@ -54,7 +55,7 @@ const register = async (username, password, studentID) => {
         studentID: studentID,
       });
       await newUser.save();
-      return {code: 200, data: newUser};
+      return { code: 200, data: newUser };
     } else {
       const saltRounds = 10;
       const hashPass = await bcrypt.hash(password, saltRounds);
@@ -65,11 +66,31 @@ const register = async (username, password, studentID) => {
         isAdmin: true,
       });
       await newUser.save();
-      return {code: 200, data: newUser};
+      return { code: 200, data: newUser };
     }
   } catch (err) {
-    return {code: 400, data: err};
+    return { code: 400, data: err };
   }
 };
+const registerPromise = (data) => {
+  let { username, password, studentID } = data;
+  return Users.findOne({ username }).then(async user => {
+    if (user) return Promise.reject({ code: 10, messageDev: "username existed!" });
+    if (studentID != null && isObjectId(studentID)) {
+      const student = await Student.findById(studentID);
+      if (student) return Promise.reject({ code: 11, messageDev: "student existed" });
+    } else {
+      studentID = mongoose.Types.ObjectId();
+    }
+    const saltRounds = 10;
+    const hashPass = await bcrypt.hash(password, saltRounds);
+    const newUser = new Users({
+      username,
+      password: hashPass,
+      studentID
+    });
+    return newUser.save();
+  });
+};
 
-module.exports = { update, changePass, register };
+module.exports = { update, changePass, register, registerPromise };
